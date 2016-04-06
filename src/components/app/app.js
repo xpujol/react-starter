f6ac35335829d61b23d2bdec788cfe5f9b82efb5
Carles Pagès <cpages@userzoom.com>
2016-04-06 13:49:39 +0200

    List component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,11 +2,21 @@
 
 import React, { Component } from 'react';
 import { Login } from '../login/login';
+import { List } from '../list/list';
 
-export const App = (props) => {
-  return (
-    <div className="app">
-      <Login/>
-    </div>
-  );
-};
+export class App extends Component {
+  constructor() {
+    super();
+    this.state = { logged: true };
+  }
+  signIn() {
+    this.setState({ logged: true });
+  }
+  render() {
+    return (
+      <div className="app">
+        {this.state.logged ? <List /> : <Login signIn={this.signIn.bind(this)} />}
+      </div>
+    );
+  }
+}
